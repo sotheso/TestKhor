9e53ad8510e247375b72858a446578ffc15ff28e
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-06-08 06:07:09 -0700

    advanced express & middleware

--- appKhor.js
+++ appKhor.js
@@ -23,15 +23,49 @@
 // ادپدیت کردن ورژن پکیج (تمام پکیج ها یکجا)
 // npm update
 
+// impurt function
+const { extend } = require("underscore")
+const logger = require("./logger")
 const express = require("express")
+const helmet = require("helmet")
+const morgon = require("morgan")
 
 // To access the environment variable
 require('dotenv').config()
+const startdebug = require("debug")("startup")
 
 const app = express()
 
-// for get body POST
+//Built-in middleware---------------------------
+// Built-in middleware function for get body POST
 app.use(express.json())
+
+// برای یکسری درخواست‌های قدیمی ارسال باشه با فرمت قدیمی
+// key=value&kay2=value => JSON
+// for warnning :body-parser deprecated undefined extended: provide extended option appKhor.js:41:17 => {extended:true}
+app.use(express.urlencoded({extended:true}))
+
+// for static file (در کدارم فولدر قرار داره)
+app.use(express.static('public'))
+
+// third party middleware--------------------- impurt middlewar Express.com
+// middleware for security
+app.use(helmet())
+
+startdebug("asdfsdfa")
+// middlewar for information
+// app.use(morgon("tiny"))
+if (app.get("env") === "development") app.use(morgon("tiny"))
+
+// middlewar for debug = console.log 
+
+
+// custom middleware----------------------------
+app.use(logger.log)
+app.use((req,res,next)=>{
+    console.log("authentication...")
+    next()
+})
 
 // Data Base
 const Papers=[
